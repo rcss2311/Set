3f784949f4eb54f088240543ad7e285310ff50da
Ricardo Souza <rcss2311@gmail.com>
2020-11-11 20:45:17 -0300

    Exercicio proposto Solução do professor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,50 +1,47 @@
 package application;
 
+import java.text.ParseException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-import entities.Usuario;
-
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
 		Scanner sc = new Scanner(System.in);
+
+		Set<Integer> a = new HashSet<>();
+		Set<Integer> b = new HashSet<>();
+		Set<Integer> c = new HashSet<>();
 		
-		
-		Set<Usuario> set = new HashSet<>();
-		
-		System.out.print("How many students in couse A: ");
-		int qty = sc.nextInt();
-		for(int i=0; i<qty;i++) {
-			int mat = sc.nextInt();
-			set.add(new Usuario(mat));
+		System.out.print("How many students for course A? ");
+		int n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			a.add(number);
+		}
+
+		System.out.print("How many students for course B? ");
+		n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			b.add(number);
+		}
+
+		System.out.print("How many students for course C? ");
+		n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			c.add(number);
 		}
 		
-		System.out.println();
-		
-		System.out.print("How many students in couse B: ");
-		qty = sc.nextInt();
-		for(int i=0; i<qty;i++) {
-			int mat = sc.nextInt();
-			set.add(new Usuario(mat));
-		}
-		
-		System.out.println();
-		
-		System.out.print("How many students in couse c: ");
-		qty = sc.nextInt();
-		for(int i=0; i<qty;i++) {
-			int mat = sc.nextInt();
-			set.add(new Usuario(mat));
-		}
-		
-		System.out.println();
-		
-		System.out.print("Total Users: "+ set.size());
-		
+		Set<Integer> total = new HashSet<>(a);
+		total.addAll(b);
+		total.addAll(c);
+
+		System.out.println("Total students: " + total.size());
+
 		sc.close();
 	}
-
 }
