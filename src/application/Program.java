8251807048412751eeb07b3e67a45095d0c42c4d
Ricardo Souza <rcss2311@gmail.com>
2020-11-11 19:09:04 -0300

    Demo 2: Operações de conjunto

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -8,18 +8,20 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Set<String> set = new LinkedHashSet<>();// mantem a ordem inserida
-		set.add("Tv");
-		set.add("Notebook");
-		set.add("Tablet");
-		
-		//set.removeIf(x -> x.length() >= 3);
-		set.removeIf(x -> x.charAt(0) == 'T');
-		
-		System.out.println(set.contains("Notebook")); // testa se existe retorna booleano
-			for (String p : set) {
-				System.out.println(p);
-		}
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
+		// union
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
+		// intersection
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println(d);
+		// difference
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
 
 	}
 
