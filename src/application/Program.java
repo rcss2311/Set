9517694fe8582a5a23a684da53d424509bf1f631
Ricardo Souza <rcss2311@gmail.com>
2020-11-11 19:29:42 -0300

    Demo 3: Como TreeSet faz a ordenação e comparação

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,28 @@
 package application;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
-		//implementando os metodos equals e hashCode na classe product, obten-se comparação por conteudo
-		//dando verdadeiro, caso não tenha a operação sera falsa 
-		Set<Product> set = new HashSet<>();
+		
+		/* Aqui é preciso implementar o compareble na classe product, adicionar o metodo toComparable para 
+		 * obter a ordenação dos itens pelo TreeSet.
+		 */
+		
+		Set<Product> set = new TreeSet<>();
 		
 		set.add(new Product("TV", 900.0));
 		set.add(new Product("Notebook", 1200.0));
 		set.add(new Product("Tablet", 400.0));
 		
-		Product prod = new Product("Notebook", 1200.0);
+		for(Product p: set) {
+			System.out.println(p);
+		}
 		
-		System.out.println(set.contains(prod));
 	}
 
 }
