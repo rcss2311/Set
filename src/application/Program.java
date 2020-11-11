242a57115fde5ceb7f2a6b9229d5040d0f5abebc
Ricardo Souza <rcss2311@gmail.com>
2020-11-11 18:56:41 -0300

    Set/TreeSet

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,17 @@
 package application;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Set<String> set = new HashSet<>();
-		set.add("TV");
+		Set<String> set = new TreeSet<>();
+		set.add("Tv");
 		set.add("Notebook");
 		set.add("Tablet");
-		System.out.println(set.contains("Notebook"));
+		System.out.println(set.contains("Notebook")); // testa se existe retorna booleano
 			for (String p : set) {
 				System.out.println(p);
 		}
