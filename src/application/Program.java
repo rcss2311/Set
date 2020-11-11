643d98ccd3407a86061b40905d2775bbd2723bd4
Ricardo Souza <rcss2311@gmail.com>
2020-11-11 19:54:33 -0300

    Problema resolvido(Exercicio)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,47 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
 
-import entities.Product;
+import entities.LogEntry;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
 		
-		/* Aqui é preciso implementar o compareble na classe product, adicionar o metodo toComparable para 
-		 * obter a ordenação dos itens pelo TreeSet.
-		 */
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
 		
-		Set<Product> set = new TreeSet<>();
-		
-		set.add(new Product("TV", 900.0));
-		set.add(new Product("Notebook", 1200.0));
-		set.add(new Product("Tablet", 400.0));
-		
-		for(Product p: set) {
-			System.out.println(p);
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			Set<LogEntry> set = new HashSet<>();
+			
+			String line = br.readLine();
+			while(line != null) {
+				String [] fields = line.split(" ");
+				String userName = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+				
+				set.add(new LogEntry(userName, moment));
+				
+				line = br.readLine();
+			}
+			System.out.println("Total users: "+set.size());
+			
+		}
+		catch (IOException e) {
+			System.out.print("Error: "+ e.getMessage());
 		}
 		
+		sc.close();
 	}
 
 }
