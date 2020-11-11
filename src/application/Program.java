a35116ef7e42d22520a2e48b9485ed4a6bd62c89
Ricardo Souza <rcss2311@gmail.com>
2020-11-11 19:04:11 -0300

    Set/set.RemoveIf(predicado)

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,8 @@ public class Program {
 		set.add("Notebook");
 		set.add("Tablet");
 		
-		set.remove("Tablet");
+		//set.removeIf(x -> x.length() >= 3);
+		set.removeIf(x -> x.charAt(0) == 'T');
 		
 		System.out.println(set.contains("Notebook")); // testa se existe retorna booleano
 			for (String p : set) {
